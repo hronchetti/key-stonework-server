edf845bc19bd421b9b516d33d496cb1e9859d40b
Harry Ronchetti <harry@ne6.digital>
2020-06-12 00:03:53 +0100

    removing email stuff as not needed with Netlify

--- app.js
+++ app.js
@@ -1,5 +1,4 @@
 import express from 'express'
-import nodemailer from 'nodemailer'
 import CORS from 'cors'
 import multer from 'multer'
 import fileSystem from 'file-system'
@@ -25,88 +24,6 @@ const CORSOptions = {
       : callback(new Error('Not allowed by CORS'))
   },
 }
-
-// CONTACT FORM SEND EMAIL
-
-const transporter = nodemailer.createTransport({
-  service: 'gmail',
-  port: 587,
-  secure: false,
-  auth: {
-    user: process.env.EMAIL_USER,
-    pass: process.env.EMAIL_PASSWORD,
-  },
-})
-
-app.post('/sendEmail', CORS(CORSOptions), (req, res) => {
-  let mailOptions = {
-    from: req.body.email,
-    to: 'harryronchetti@gmail.com',
-    subject: 'Key stonework website',
-    html: `
-    <table>
-      <tbody>
-        <tr>
-          <td colspan="2"><h2>New Contact Form Response</h2></td>
-        </tr>
-        <tr>
-          <td><strong>Name:</strong></td>
-          <td>${req.body.name}</td>
-        </tr>
-        <tr>
-          <td><strong>Phone:</strong></td>
-          <td>${req.body.phone}</td>
-        </tr>
-        <tr>
-          <td><strong>Email:</strong></td>
-          <td>${req.body.email}</td>
-        </tr>
-        <tr>
-          <td><strong>Message:</strong></td>
-          <td>${req.body.message}</td>
-        </tr>
-        <tr>
-          <td><strong>Preferred stone colour:</strong></td>
-          <td>
-            ${req.body.stoneColour ? req.body.stoneColour : 'None specified'}
-          </td>
-        </tr>
-        <tr>
-          <td>
-            <strong>Interested in:</strong>
-          </td>
-          <td>
-            ${req.body.architecturalPieces ? 'Architectural pieces,' : ''}
-            ${req.body.ballsCollardBases ? 'Balls & collard bases,' : ''}
-            ${req.body.balustrading ? 'Balustrading,' : ''}
-            ${req.body.corbels ? 'Corbels,' : ''}
-            ${req.body.keystones ? 'Keystones,' : ''}
-            ${req.body.pierCaps ? 'Pier caps,' : ''}
-            ${req.body.porticos ? 'Porticos,' : ''}
-            ${req.body.quions ? 'Quions,' : ''}
-            ${req.body.stringsPlinths ? 'Strings & plinths,' : ''}
-            ${req.body.wallCoping ? 'Wall coping,' : ''}
-            ${req.body.windowCillsHeads ? 'Window cills & heads,' : ''}
-            ${req.body.windowSurrounds ? 'Window surrounds,' : ''}
-          </td>
-        </tr>
-      </tbody>
-    </table>`,
-  }
-
-  if (req.body.email && req.body.name && req.body.message && req.body.phone) {
-    transporter
-      .sendMail(mailOptions)
-      .then(() => {
-        res.json({ message: 'Success' })
-      })
-      .catch(() => {
-        res.json({ message: 'Error' })
-      })
-  } else {
-    res.json({ message: 'User error' })
-  }
-})
 
 // ADMIN LOGIN
 
